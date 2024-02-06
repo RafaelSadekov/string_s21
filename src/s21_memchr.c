39663541de94458e99cc571b243320ed2ce1008f
RafHP <rafaelsadekov@gmail.com>
2024-02-06 13:20:11 +0300

    gylberdo

--- src/s21_memchr.c
+++ src/s21_memchr.c
@@ -2,6 +2,7 @@
 
 // Выполняет поиск первого вхождения символа c (беззнаковый тип) в первых n
 // байтах строки, на которую указывает аргумент str.
+//@gylberdo
 void* s21_memchr(const void* str, int c, size_t count) {
   char* out = NULL;
   char* temp = (char*)str;
@@ -15,3 +16,15 @@ void* s21_memchr(const void* str, int c, size_t count) {
 
 // возвращает указатель. В вашем случае, она возвращает указатель типа void *,
 // что соответствует типу, используемому в стандартной функции memchr.
+
+/*
+void *s21_memchr(const void *str, int c, size_t count) {
+    const unsigned char *p = str;
+    for (size_t i = 0; i < count; i++) {
+        if (p[i] == (unsigned char)c) {
+            return (void *)(p + i);
+        }
+    }
+    return NULL;
+}
+*/
