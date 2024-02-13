2cae66498a5a43d3eab1cdfa216e62d7ca77aeb6
RafHP <rafaelsadekov@gmail.com>
2024-02-13 18:43:18 +0300

    test strrchr

--- src/s21_main_test.c
+++ src/s21_main_test.c
@@ -449,13 +449,57 @@ Suite *suite_strpbrk(void) {
   Suite *s8 = suite_create("s21_strpbrk");
   TCase *tc_strpbrk = tcase_create("case_s21_strncmp");
 
-  tcase_add_test(tc_strncmp, strpbrk_found);
-  tcase_add_test(tc_strncmp, strpbrk_not_found);
-  tcase_add_test(tc_strncmp, strpbrk_empty_str1);
-  tcase_add_test(tc_strncmp, strpbrk_empty_str2);
-  tcase_add_test(tc_strncmp, strpbrk_long_str2);
+  tcase_add_test(tc_strpbrk, strpbrk_found);
+  tcase_add_test(tc_strpbrk, strpbrk_not_found);
+  tcase_add_test(tc_strpbrk, strpbrk_empty_str1);
+  tcase_add_test(tc_strpbrk, strpbrk_empty_str2);
+  tcase_add_test(tc_strpbrk, strpbrk_long_str2);
 
   suite_add_tcase(s8, tc_strpbrk);
+  return s8;
+}
+
+START_TEST(strrchr_last_occurrence) {
+  const char *str = "find the last occurrence of character";
+  ck_assert_ptr_eq(s21_strrchr(str, 'c'), strrchr(str, 'c'));
+}
+END_TEST
+
+START_TEST(strrchr_not_present) {
+  const char *str = "no such character here";
+  ck_assert_ptr_eq(s21_strrchr(str, 'z'), NULL);
+}
+END_TEST
+
+START_TEST(strrchr_null_char) {
+  const char *str = "searching for the end";
+  ck_assert_ptr_eq(s21_strrchr(str, '\0'), strrchr(str, '\0'));
+}
+END_TEST
+
+START_TEST(strrchr_empty_string) {
+  const char *str = "";
+  ck_assert_ptr_eq(s21_strrchr(str, 'a'), NULL);
+}
+END_TEST
+
+START_TEST(strrchr_only_char) {
+  const char *str = "aaaa";
+  ck_assert_ptr_eq(s21_strrchr(str, 'a'), strrchr(str, 'a'));
+}
+END_TEST
+
+Suite *suite_strrchr(void) {
+  Suite *s8 = suite_create("s21_strrchrk");
+  TCase *tc_strrchr = tcase_create("case_s21_strrchrp");
+
+  tcase_add_test(tc_strrchr, strrchr_last_occurrence);
+  tcase_add_test(tc_strrchr, strrchr_not_present);
+  tcase_add_test(tc_strrchr, strrchr_null_char);
+  tcase_add_test(tc_strrchr, strrchr_empty_string);
+  tcase_add_test(tc_strrchr, strrchr_only_char);
+
+  suite_add_tcase(s8, strrchr);
   return s8;
 }
 
@@ -471,6 +515,7 @@ int main(void) {
   srunner_add_suite(sr, suite_strchr());
   srunner_add_suite(sr, suite_strncmp());
   srunner_add_suite(sr, suite_strpbrk());
+  srunner_add_suite(sr, suite_strrchr());
 
   srunner_run_all(sr, CK_NORMAL);  // Запускаем все тесты в сюите
   number_failed =
